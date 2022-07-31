1e0cdba7b72483f8e90d0dbd8cca2d0089f8cfb2
csm-kr <csm8167@naver.com>
2022-07-31 15:23:07 +0900

    add device at .yaml file

--- configs/config.py
+++ configs/config.py
@@ -2,9 +2,9 @@ import argparse
 import torch
 import os
 
-device_ids = [0]
-device = torch.device('cuda:{}'.format(min(device_ids))
-                      if torch.cuda.is_available() else 'cpu')
+# device_ids = [0]
+# device = torch.device('cuda:{}'.format(min(device_ids))
+#                       if torch.cuda.is_available() else 'cpu')
 
 CONFIG_DIR = os.path.dirname(os.path.realpath(__file__))
 LOG_DIR = os.path.join(os.path.abspath(os.path.dirname(
@@ -12,7 +12,7 @@ LOG_DIR = os.path.join(os.path.abspath(os.path.dirname(
 
 # [chair, drums, ficus, hotdog, lego, materials, mic, ship]
 # FIXME Something wrong with ficus & ship
-DATA_NAME = 'hotdog'
+DATA_NAME = 'lego'
 
 print('>> CONFIG File PATH : {}'.format(CONFIG_DIR))
 print('>> LOG_DIR File PATH : {}'.format(LOG_DIR))
